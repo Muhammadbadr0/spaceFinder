9d9d9169b1d64fd5b7b498388e29275b7cf2d6ba
MuhammadBadr <Muhammadbadr98@protonmail.com>
2021-07-28 22:04:59 +0200

    add application error handler.

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
+
+/* === MIDDLEWARE === */
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+/* === ROUTES === */
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
